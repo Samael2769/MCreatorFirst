497a8c300456e26c7d1c5f096508796c0915d3b0
Samael <jeremie.joannard@epitech.eu>
2023-06-12 07:01:51 +0200

    [MOD] .gitignore, [MOD] README.md, [ADD] Assets/, [ADD] FirstMode/,
    Adding texture and Model for the Vulkan entity

---
+++ FirstMode/src/main/java/net/mcreator/firstmode/client/model/ModelVulkan.java
@@ -0,0 +1,55 @@
+package net.mcreator.firstmode.client.model;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.EntityModel;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.PoseStack;
+
+// Made with Blockbench 4.7.4
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+public class ModelVulkan<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in
+	// the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("firstmode", "model_vulkan"), "main");
+	public final ModelPart bb_main;
+
+	public ModelVulkan(ModelPart root) {
+		this.bb_main = root.getChild("bb_main");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main",
+				CubeListBuilder.create().texOffs(-13, -13).addBox(-3.0F, -3.5F, -10.0F, 6.0F, 2.5F, 15.0F, new CubeDeformation(0.0F)).texOffs(0, 0).addBox(-5.0F, -3.0F, 0.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-5.0F, -3.0F, -7.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0).addBox(3.0F, -3.0F, -7.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(3.0F, -3.0F, 0.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0).addBox(-4.5F, -3.7F, 0.0F, 1.5F, 0.7F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-4.5F, -3.7F, -7.0F, 1.5F, 0.7F, 2.0F, new CubeDeformation(0.0F)).texOffs(1, 1).addBox(-2.0F, -4.0F, 5.0F, 4.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(-2, -2)
+						.addBox(-1.75F, -2.0F, 6.0F, 3.5F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(-6, -6).addBox(-2.0F, -2.0F, -18.0F, 4.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(1, 1)
+						.addBox(0.5F, -3.0F, 5.0F, 0.5F, 0.5F, 1.0F, new CubeDeformation(0.0F)).texOffs(1, 1).addBox(-1.0F, -3.0F, 5.0F, 0.5F, 0.5F, 1.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition cube_r1 = bb_main.addOrReplaceChild("cube_r1",
+				CubeListBuilder.create().texOffs(0, 0).addBox(-4.7F, -3.7F, 6.0F, 1.7F, 0.7F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0).addBox(-4.7F, -3.7F, -1.0F, 1.7F, 0.7F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offsetAndRotation(0.0F, 0.0F, 1.0F, 0.0F, 3.1416F, 0.0F));
+		return LayerDefinition.create(meshdefinition, 16, 16);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+	}
+}
